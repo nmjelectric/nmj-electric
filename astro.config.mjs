ef273579d42776883616603487155431bb375516
Mario Gonzales <mario.gonzales@nmjelectric.com>
2026-07-19 21:14:28 -0500

    created hamburger menu for mobile, added linkable cards to services, added tecl number to footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  site: 'https://nmjelectric.com',
+
+  adapter: cloudflare(),
+
+  integrations: [sitemap()]
 });
